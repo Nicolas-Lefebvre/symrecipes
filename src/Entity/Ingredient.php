14570f464fdc0ce8e805885d2368bddd242a801c
Nicolas-Lefebvre <nicolas.ap.lefebvre@gmail.com>
2024-01-21 21:06:58 +0100

    Correction Redirection to route après ajout

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use App\Repository\IngredientRepository;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
+// Pour éviter les doublons sur le nom des ingrédients
+#[UniqueEntity('name')]
 class Ingredient
 {
     #[ORM\Id]
